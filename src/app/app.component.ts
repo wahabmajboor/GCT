5e20e77622401f6540b144b4c783ee98a3b6bd3c
wahabmajboor <wahabmajboor@yahoo.com>
2017-04-23 12:29:45 +0200

    Revert "test twee bestanden"
    This reverts commit a5f86fe5a6effaa30970e3a9d065aa7f2f3d5044.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works great Wahab eerste versie twee bestanden!';
+  title = 'app works great Wahab eerste versie!';
 }
